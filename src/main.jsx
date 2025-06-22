d2aef78e955f75934ae2e3b0b53f8811d8ed712e
neiljya <neil.j.wu@gmail.com>
2025-06-21 17:21:21 -0700

    fixed styling and merge conflict thigns

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-=======
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -23,4 +10,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 );
->>>>>>> 9e59f77 (another one)
+
